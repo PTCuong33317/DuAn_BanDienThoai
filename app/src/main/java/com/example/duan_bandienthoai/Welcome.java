fe411bc0482e0b2b9a0f6d5d815167986cc895f5
PTCuong33317 <cuongptpheee17@fpt.edu.vn>
2023-11-18 15:57:58 +0700

    welcome and đổi mk

---
+++ app/src/main/java/com/example/duan_bandienthoai/Welcome.java
@@ -0,0 +1,15 @@
+package com.example.duan_bandienthoai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+    }
+}
